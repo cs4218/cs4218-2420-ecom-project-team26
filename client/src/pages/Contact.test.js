b6115db803c33482fcf09c591cb926944538987c
JonathanChongLyenChyn <jonchong98@gmail.com>
2025-03-09 12:31:36 +0800

    Add unit test for Contact.js

---
+++ client/src/pages/Contact.test.js
@@ -0,0 +1,51 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import Contact from "./Contact";
+import React from "react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+
+jest.mock(
+  "../components/Layout",
+  () =>
+    ({ children, title, description, keywords, author }) =>
+      (
+        <div>
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <meta name="author" content={author} />
+          <title>{title}</title>
+          <main>{children}</main>
+        </div>
+      )
+);
+
+describe("Contact Component", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders contact page with correct content", async () => {
+    const { getByAltText, getByText } = render(
+      <MemoryRouter initialEntries={["/contact"]}>
+        <Routes>
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(getByText("CONTACT US")).toBeInTheDocument();
+    expect(
+      getByText(
+        "For any query or info about product, feel free to call anytime. We are available 24X7."
+      )
+    ).toBeInTheDocument();
+
+    // Since the icons and text are separate elements, use text with a regex or partial match
+    expect(getByText(/www\.help@ecommerceapp\.com/)).toBeInTheDocument();
+    expect(getByText(/012-3456789/)).toBeInTheDocument();
+    expect(getByText(/1800-0000-0000 \(toll free\)/)).toBeInTheDocument();
+
+    // Verifying the image alt text
+    expect(getByAltText("contactus")).toBeInTheDocument();
+  });
+});
